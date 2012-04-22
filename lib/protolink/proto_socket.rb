37498ee7557f01974f2a6485f974f9404c2d9ff8
Ali Jelveh <jelveh@web.de>
2012-04-22 22:20:37 +0200

    * handle sudden disconnects gracefully

--- lib/protolink/proto_socket.rb
+++ lib/protolink/proto_socket.rb
@@ -13,9 +13,14 @@ class ProtoSocket < FlashConnection
               :payload => {:type => 'web', :user_id => @connection.current_user.id, :token => token}
     
     periodic_ping
+    disconnect_if_no_ping_received
   end
   
   def receive_json(json)
+    if json["trigger"] == "socket.ping_received"
+      @last_ping_received = Time.now
+      return
+    end
     @message_callback.call(json)
   end
   
@@ -25,10 +30,19 @@ class ProtoSocket < FlashConnection
       send_json :operation => 'ping'
     end
   end
+
+  def disconnect_if_no_ping_received
+    @ping_check ||= EventMachine.add_periodic_timer 30 do
+      if @last_ping_received && @last_ping_received < (Time.now - 60)
+        close_connection
+      end      
+    end
+  end
   
   def unbind
     puts "DISCONNECTED"
     EventMachine::cancel_timer @ping
+    EventMachine::cancel_timer @ping_check
     EventMachine::add_timer(30) {
       EventMachine.connect URI.parse(@connection.class.base_uri).host, 5000, ProtoSocket, @connection, @message_callback
     }
